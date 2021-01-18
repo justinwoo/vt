fb08ae9a5f0d8fe1ea263e061af743aa451d5ab5
justinwoo <moomoowoo@gmail.com>
2021-01-18 18:13:21 +0200

    support old regex

--- src/NameParser.js
+++ src/NameParser.js
@@ -1,12 +1,19 @@
 exports.parseTitle_ = (nothing) => (just) => (input) => {
   let regex = /\[.*\] (.+) - ([\d]+)[v\d]* \(.*\) \[.*\]\.[\w]*/;
+  let old_regex = /\[.*\] (.+) - (\d+) (?:\[.*\])*.mkv/;
   let matches = input.match(regex);
-  if (!matches || matches.length != 3) {
-    return nothing;
-  } else {
+  let old_matches = input.match(old_regex);
+  if (matches && matches.length === 3) {
     return just({
       name: matches[1],
       episode: parseInt(matches[2]),
     });
+  } else if (old_matches && old_matches.length === 3) {
+    return just({
+      name: old_matches[1],
+      episode: parseInt(old_matches[2]),
+    });
+  } else {
+    return nothing;
   }
 };
