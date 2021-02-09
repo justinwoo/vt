d361bb9474d0824a7ef280b8b06280da4c35a0c2
justinwoo <moomoowoo@gmail.com>
2021-02-10 01:39:49 +0200

    of course you can't use Int

--- src/FrontEnd/Window.purs
+++ src/FrontEnd/Window.purs
@@ -16,13 +16,13 @@ foreign import addWindowKeyListener :: (String -> Effect Unit) -> Effect Unit
 foreign import refreshPage :: Effect Unit
 foreign import scrollIntoView :: Path -> Effect Unit
 foreign import scrollToTop :: Effect Unit
-foreign import getDateTime :: Effect Int
+foreign import getDateTime :: Effect Number
 
 window :: Event ScrollEvent -> Effect (Event KeyboardEvent)
 window scrollEvent = do
   {event, push} <- Event.create
 
-  prevTimeRef <- Ref.new 0
+  prevTimeRef <- Ref.new 0.0
 
   addWindowKeyListener \key -> do
     let
@@ -44,12 +44,12 @@ window scrollEvent = do
 
     let timediff = currTime - prevTime
 
-    guard (timediff > 40) do
+    guard (timediff > 40.0) do
       traverse_ push mAction
       Ref.modify_ (\_ -> currTime) prevTimeRef
 
   void $ Event.subscribe scrollEvent case _ of
-    ScrollFileIntoView file ->
+    ScrollFileIntoView file -> do
       scrollIntoView file.name
 
   pure event
